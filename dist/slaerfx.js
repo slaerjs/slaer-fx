68f5d71f40bfa8c8fdaac3b9509376ed1120ae86
Gary Doublé <garydouble@live.com>
2017-05-03 03:11:23 +0100

    a little work on some super basic phyics experiments

--- dist/slaerfx.js
+++ dist/slaerfx.js
@@ -183,14 +183,24 @@ function start() {
   var _objects = objects();
   var _behaviours = behaviours();
 
+  var now = new Date().getTime(), then = now, dt = 0;
+
   (function loop() {
+    then = now;
+    now = new Date().getTime();
+    dt = Math.min((now - then) / 1000.0, 0.1);
+
+    var canvas = document.getElementsByTagName('canvas')[0];
+    canvas.width = canvas.width;
+    canvas.height = canvas.height;
+
     for (var key in _objects) {
       for (var component in _objects[key].components) {
-        if (typeof component === 'function') {
-          component.call(_objects[key]);
+        if (_behaviours[component]) {
+          _behaviours[component].call(_objects[key], dt, _objects[key].components[component]);
         }
-        else if (_behaviours[component]) {
-          _behaviours[component].call(_objects[key], _objects[key].components[component]);
+        else if (typeof component === 'function') {
+          component.call(_objects[key], dt);
         }
         else {
           console.warn('Unknown behaviour ' + component);
