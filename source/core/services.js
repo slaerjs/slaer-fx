af2b3070bd6becc1bd8cbab79013e386aa34f482
Gary Doublé <garydouble@live.com>
2017-06-01 01:43:19 +0100

    added services shell file

---
+++ source/core/services.js
@@ -0,0 +1,11 @@
+/**********************************************************
+Slaer Services
+
+A service represents is a piece of encapulated functionality
+that is called upon to perform some specific task. All
+services run "on-demand" and as such are only invoked when
+explicitly requested to do so or via an event mapping.
+***********************************************************/
+export function service() {
+
+}
